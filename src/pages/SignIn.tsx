e7e04b6d3021aba154fe55c93a8751a56ad1e320
shrihari <shrihari@perspectiv.in>
2024-12-27 17:52:44 +0530

    add RegisteredUsers page and navigation from SignIn

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -4,12 +4,23 @@ import SignInQuote from '../assets/SignInQuote.svg';
 import SignInBenifits from '../assets/Benefits.svg';
 import BackgroundPattern3 from '../assets/Background_Pattern_3.svg';
 import sandhyaTimeLogo from '../assets/SandhyaTime-Logo.svg';
+import { useNavigate } from 'react-router-dom';
+
 function SignIn() {
+  const navigate = useNavigate();
+
+  const handleTouchStart = () => {
+    setTimeout(() => {
+      navigate('/registered-users');
+    }, 6000);
+  };
   return (
     <div className="fixed inset-0 flex items-center justify-center">
       <div
         className=" flex flex-col justify-between w-full h-full max-w-lg relative  bg-[#532C16] bg-center bg-cover"
         style={{ backgroundImage: `linear-gradient(rgba(83, 44, 22, 0.8),rgba(83, 44, 22, 0.8)),url(${BackgroundPattern3})` }}
+        onMouseDown={handleTouchStart}
+        onTouchStart={handleTouchStart}
       >
         <div className="flex justify-center items-center mt-10 relative z-10">
           <img src={sandhyaTimeLogo} alt="Sandhya Time Logo" className="w-30 h-auto" />
