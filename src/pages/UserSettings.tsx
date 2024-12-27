6a94054cf9dfba1c72dc8dd0fe625bcab9fc23b9
Hegde07 <hegdebhargav92@gmail.com>
2024-12-27 06:39:59 +0530

    Color fix

--- src/pages/UserSettings.tsx
+++ src/pages/UserSettings.tsx
@@ -81,7 +81,7 @@ function UserSettings() {
         <div className="flex justify-center items-center mt-10 relative z-10">
           <img src={sandhyaTimeLogo} alt="Sandhya Time Logo" className="w-32 h-auto" />
         </div>
-        <div className="relative z-10 p-6">
+        <Block className="relative z-10 p-6">
           <List strongIos insetIos style={{ margin: 0 }}>
             <div>
               <p className="text-white mb-2">Email</p>
@@ -216,7 +216,7 @@ function UserSettings() {
               Save
             </Button>
           </Block>
-        </div>
+        </Block>
       </div>
     </div>
   );
