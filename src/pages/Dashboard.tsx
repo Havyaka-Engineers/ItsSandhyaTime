8a595ac8f3aa7689eb08005a55c4f1330d479a44
shrihari <shrihari@perspectiv.in>
2024-12-29 11:11:27 +0530

    refactor: comment out useNavigate import in Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 // import { Block } from 'konsta/react';
 import { SessionSettings } from '../types/SessionSettings';
 import { Lesson } from '../types/Lesson';
